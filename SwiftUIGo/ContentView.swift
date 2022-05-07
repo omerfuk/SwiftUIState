d2d29e4d91d51213c447bfe807e558731123435a
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-07 21:46:32 +0300

    Initial Commit

---
+++ SwiftUIGo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGo
+//
+//  Created by Ömer Faruk Kılıçaslan on 7.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGo/SwiftUIGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGoApp.swift
+//  SwiftUIGo
+//
+//  Created by Ömer Faruk Kılıçaslan on 7.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
